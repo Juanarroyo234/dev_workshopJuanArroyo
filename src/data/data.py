5602c2661cacadcc6c39c4a6ee32bded450f1df2
juan <jearroyo27@ucatolica.edu.co>
2025-03-05 19:23:25 -0500

    buscar elemento

--- src/data/data.py
+++ src/data/data.py
@@ -31,7 +31,10 @@ class Data:
         Returns:
             int: Índice del elemento o -1 si no se encuentra
         """
-        pass
+         for i in range(len(lista)):  # Recorremos la lista con un índice
+        if lista[i] == elemento:  # Si encontramos el elemento
+            return i  # Retornamos su índice
+    return -1
     
     def eliminar_duplicados(self, lista):
         """
