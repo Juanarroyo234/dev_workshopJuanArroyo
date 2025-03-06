a393135178c963247524fb94662032a04b411762
juan <jearroyo27@ucatolica.edu.co>
2025-03-05 23:16:28 -0500

    volumen cilindro

--- src/geometric/geometric.py
+++ src/geometric/geometric.py
@@ -235,7 +235,7 @@ class Geometria:
         Returns:
             float: Volumen del cilindro
         """
-        pass
+        return  math.pi * (radio ** 2) * altura
     
     def area_superficie_cilindro(self, radio, altura):
         """
